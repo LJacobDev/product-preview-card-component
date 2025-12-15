38646c6dfd56cfec2db483ce8f1e2da94f4f97d4
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2025-12-14 20:18:26 -0500

    Set up actions test, build, deploy workflow test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,14 @@
-// import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest'
 
-// import { mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 // import HelloWorld from '../HelloWorld.vue'
 
-// describe('HelloWorld', () => {
-//   it('renders properly', () => {
-//     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-//     expect(wrapper.text()).toContain('Hello Vitest')
-//   })
-// })
+describe('HelloWorld', () => {
+  it.skip('renders properly', () => {
+    // const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    // expect(wrapper.text()).toContain('Hello Vitest')
+  })
+  it('passes for debug purposes', () => {
+    expect(true).toBe(true)
+  })
+})
